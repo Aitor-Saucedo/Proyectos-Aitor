fd9c573f53cedc1cfaa7e46233795d4e38e1c999
Aitor-Saucedo <61379057+Aitor-Saucedo@users.noreply.github.com>
2020-02-23 14:37:12 +0100

    grid css y ejercicios css y html

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-jQuery('document').ready(function($)){
+var nombre; //string nombre de usuario
+var fecha = new Date(); //fecha y hora actual
+var hora = fecha.getHours(); //hora actual formato 24 horas
+var minuto = fecha.getMinutes();
 
-    var menuBtn = $('.menu-icon'),
-        menu = $('.navigation ul');
-    menuBtn.click(function() {
+//desde el promp guarda el nombre de la variable nombre
+nombre = window.prompt("Introduzca su nombre");
 
-        if(menu.hasClass('show') ) {
-            menu.removeClass ('show');
-        }   else {
-
-            menu.addClass('show');
-
-        }
-
-    });
-
-
-});
+document.writeln(nombre + " son las: " + hora + " h " + minuto + "min" + bienvenido/a mi web);
